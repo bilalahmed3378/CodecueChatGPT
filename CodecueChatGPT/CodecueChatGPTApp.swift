5bff9cb7fd96e43da0516ff0382aff2ceb19f4ef
bilalahmed3378 <109059774+bilalahmed3378@users.noreply.github.com>
2023-01-30 12:23:04 +0500

    Initial Commit

---
+++ CodecueChatGPT/CodecueChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodecueChatGPTApp.swift
+//  CodecueChatGPT
+//
+//  Created by Bilal Ahmed on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CodecueChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
